c197b8808932c1fc6d0ea4b55d946b6733f50f03
santilab48 <tusprc@gmail.com>
2026-08-28 09:42:59 +0700

    Connect Sangkub web app to Supabase

---
+++ lib/supabase.ts
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js'
+
+export const supabase = createClient(
+  'https://uhhfthgbtpuljelmqmpc.supabase.co',
+  'sb_publishable_u-JbcmNjLKPVrvPz9f7W1Q_GJ4KfXy7'
+)
